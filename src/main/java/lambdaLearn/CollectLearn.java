405ea4e74104e278ed755e70f8acee4887d5f1b5
zjw <1300151235@qq.com>
2019-11-01 17:09:33 +0800

    CollectionLearn添加新的代码

--- src/main/java/lambdaLearn/CollectLearn.java
+++ src/main/java/lambdaLearn/CollectLearn.java
@@ -1,5 +1,6 @@
 package lambdaLearn;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +18,7 @@ public class CollectLearn {
         //List<Double> result=list.stream().filter(n -> n>=200).collect(Collectors.toList());
 
         //可以分解为下面的步骤,便于理解
+        //将list中大于等于200的元素生成一个新的list（过滤操作）
         List<Double> result=list.stream().filter(n ->{
             System.out.println(n);
             return  n>=200? Boolean.TRUE:Boolean.FALSE;
@@ -24,6 +26,15 @@ public class CollectLearn {
 
         System.out.println("toList  result:{}"+result);
 
+        List<Double> doubleList=new ArrayList<>();
+
+        //将list中的每个元素扩大2倍后得到新的集合(对每一个元素进行同样的操作)
+        doubleList=list.stream().map(n -> {
+            System.out.println("n:"+n);
+            return 2*n;
+        }).collect(Collectors.toList());
+
+        System.out.println("doubleList:"+doubleList);
 
         List<String> stringList= Arrays.asList("java","php","c++","js");
 
