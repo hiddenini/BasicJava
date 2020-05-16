110e554aa8b3ac81e2fb33473cbedef29c77b388
bonatgy <1300151235@qq.com>
2020-05-16 11:51:09 +0800

    volatile 和sync笔记

--- src/main/java/ThreadLearn/ObjectLayout/Jol.java
+++ src/main/java/ThreadLearn/ObjectLayout/Jol.java
@@ -31,7 +31,7 @@ import org.openjdk.jol.info.ClassLayout;
  * 在HotSpot虚拟机中，对象在内存中的布局主要分为3块区域:对象头(Header),实例数据(Instance Data)
  * 和对齐填充(padding，因为hotspot要求对象的起始地址必须是8字节的整数倍)
  * <p>
- * 对象头(Header) 包括2部分信息  一部分是MarkWord 另一部分是类型指针(Class Pointer)
+ * 对象头(Header) 包括2部分信息  一部分是MarkWord 另一部分是类型指针(Klass Pointer)
  * <p>
  * MarkWord 主要存储锁信息 gc信息 hashCode
  * <p>
@@ -57,7 +57,38 @@ import org.openjdk.jol.info.ClassLayout;
  * <p>
  * 当竞争不激烈,需要执行的动作很快时适合轻量级锁
  * <p>
- * 但是竞争很激烈,需要执行的动作很耗时轻量级锁就不合适了(会有大量的线程在进行长时间的自旋)
+ * 但是竞争很激烈,需要执行的动作很耗时轻量级锁就不合适了(会有大量的线程在进行长时间的自旋,自旋锁是消费cpu资源的)
+ * <p>
+ * 偏向锁是否一定比自旋锁效率高?
+ * 不一定，在明确知道会有多线程竞争的情况下，偏向锁肯定会涉及锁撤销，这时候直接使用自旋锁
+ * JVM启动时会有很多线程竞争(明确)，所以默认情况启动时不打开偏向锁，过一段时间再打开(4s)
+ * Lock指令(汇编层面)  volatile 是 lock addl   synchronized 是 lock cmpxchg
+ * 作用在于多处理器中的执行指令时对共享内存的独占使用
+ * 它的作用是能够将当前处理器对应的缓存的内容刷新到主存，并使其他处理器对应的缓存失效
+ * 另外还提供了有序的指令无法越过这个内存屏障的作用
+ * MESI Cache 缓存一致性协议（因特尔的cpu）指的是缓存行的四种状态 Modified Exclusive Shared Invalid
+ * <p>
+ * 一个cache line Modified 之后要将另外一个使用了同样数据的cache line置为Invalid(个人描述)
+ * <p>
+ * 局部性原理  按块读取 cpu读取数据时是64(折中值,局部性空间效率和读取时间的折中)个字节一起读到缓存
+ * <p>
+ * 伪共享问题  eg 2个线程对于size为2的T数组中的T的的volatile变量x的一亿次累加
+ * <p>
+ * 下面这种的时间大概是在3s,原因是这2个x在内存中的位置是非常靠近，位于一个cache line 所以存在互相通知将自己的cache line失效的过程
+ * T{
+ * public volatile long x =0l;
+ * }
+ * <p>
+ * 下面这种的时间大概是在1s,因为x前后有占位的变量，并且字节数量为56 ,也就是说用这种方式保证了不会和其他x在同一个cache line
+ * <p>
+ * 也就保证了不会存在互相通知将缓存失效的过程
+ * T{
+ * public volatile long p1,p2,p3,p4,p5,p6,p7;
+ * public volatile long x =0l;
+ * public volatile long p9,p10,p11,p12,p13,p14,p15;
+ * }
+ * <p>
+ * wc(缓存) 合并写技术 cpu内部的一个缓存，4个字节
  */
 public class Jol {
     public static void main(String[] args) {
