9f3860b6e325e56bac5fa4dac829fd3a8d96745e
bonatgy <1300151235@qq.com>
2021-04-18 17:47:35 +0800

    map

--- src/main/java/interview/concurrentHashMap/Sight.java
+++ src/main/java/interview/concurrentHashMap/Sight.java
@@ -1,12 +1,20 @@
 package interview.concurrentHashMap;
 
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Sight {
     public static void main(String[] args) {
-        ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<>(15);
+
+        HashMap<Integer, Integer> map = new HashMap<>(8);
         for (int i = 0; i < 5; i++) {
             map.put(i, i);
+        }
+        map.get(2);
+
+        ConcurrentHashMap<Integer, Integer> concurrentHashMap = new ConcurrentHashMap<>(15);
+        for (int i = 0; i < 5; i++) {
+            concurrentHashMap.put(i, i);
         }
     }
 }
